6b6e9a0d0d5e7125c9ccfd6e944cb8f79b1dc155
minecraflover321-hub <minecraflover321@gmail.com>
2026-02-23 20:52:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,43 +1,30 @@
-"""
-Music Player, Telegram Voice Chat Bot
-Copyright (c) 2021-present Asm Safone <https://github.com/AsmSafone>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
 
 class Config:
     def __init__(self) -> None:
-        self.API_ID: str = os.environ.get("API_ID", None)
-        self.API_HASH: str = os.environ.get("API_HASH", None)
-        self.SESSION: str = os.environ.get("SESSION", None)
-        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", None)
+        # Aapki personal details yahan set hain
+        self.API_ID: str = os.environ.get("API_ID", "28906864")
+        self.API_HASH: str = os.environ.get("API_HASH", "62cfe849259339c73b6226e4a0599ed5")
+        self.BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "8255597786:AAE0qEuDheFJPiVItN3mPob95kGXodhtgeg")
+        self.SESSION: str = os.environ.get("SESSION", "BQG5FXAAC33sudBT0lawN-jr7IKmVU3wtvjaPGv1B32qeEIq8nqPONwADKlcr-ONGFRn92LRDTBMx5sO1ga5yeFpORyhBXJ073unGd8wkCTXHzGeZwfPEiiECZ1wGzVwlwUAFDcyPuKlaMTRUbFVlk8zbSKqLS99AP9ycp0VCPcSr5G81S51_bIDyvL4vzjL_M5MIlQ6A4yrEQvQV3G8Rp6v5XAcJj4VHK2oOuA-7pfOM-podPw2WfvWg3GiAPOCQL23spMbWVHQKEdq_WyU8PEB_bm2f8vScsrgNeNSl2uaPNNqxI7l8YD7eU4ikgTZouGB0RGzFfTXblmBuLPXskSaKZoCtgAAAAHaWwCuAA")
+        
+        # SUDOERS mein apni Telegram ID yahan likhein (Space dekar)
+        # Udaharan: "12345678 87654321"
         self.SUDOERS: list = [
-            int(id) for id in os.environ.get("SUDOERS", " ").split() if id.isnumeric()
+            int(id) for id in os.environ.get("SUDOERS", "7958364334").split() if id.isnumeric()
         ]
+
         if not self.SESSION or not self.API_ID or not self.API_HASH:
             print("ERROR: SESSION, API_ID and API_HASH is required!")
             quit(0)
+
+        # Basic Settings
         self.SPOTIFY: bool = False
         self.QUALITY: str = os.environ.get("QUALITY", "high").lower()
-        self.PREFIXES: list = os.environ.get("PREFIX", "!").split()
+        self.PREFIXES: list = os.environ.get("PREFIX", "! /").split()
         self.LANGUAGE: str = os.environ.get("LANGUAGE", "en").lower()
         self.STREAM_MODE: str = (
             "audio"
@@ -47,6 +34,5 @@ class Config:
         self.ADMINS_ONLY: bool = os.environ.get("ADMINS_ONLY", False)
         self.SPOTIFY_CLIENT_ID: str = os.environ.get("SPOTIFY_CLIENT_ID", None)
         self.SPOTIFY_CLIENT_SECRET: str = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
-
 
 config = Config()
